ce6b042a43e5f5f8ac3210dc72d97de6962d440d
mr-elbe5 <mr@elbe5.de>
2025-01-16 15:28:07 +0100

    Initial Commit

---
+++ OSM Exif Editor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OSM Exif Editor
+//
+//  Created by Michael Rönnau on 16.01.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
